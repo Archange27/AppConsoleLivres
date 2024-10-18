3bcdbc456042fcf10d162dacafeb346b7daa0af4
Archange <archange.tshikayakalala@monboreal.ca>
2024-10-18 14:16:09 -0400

    Ajout de la classe Roman

---
+++ AppConsoleLivres/Roman.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Roman : Livre
+    {
+        public string Genre { get; set; }
+        
+        public Roman(string Titre, string Auteur, int NombrePages, int Numero, int Annee) : base(Titre, Auteur, NombrePages)
+
+
+        {
+            Genre = Genre;
+
+        }
+        public override void AfficherDetail()
+        {
+
+            Console.WriteLine($"Genre: {Genre}");
+        }
+    }
+}
