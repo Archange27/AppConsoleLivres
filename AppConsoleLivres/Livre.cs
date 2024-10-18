bec1cde2a4be6205cca90ae2135d7befe6b718b2
Archange <archange.tshikayakalala@monboreal.ca>
2024-10-18 11:03:29 -0400

    Ajout de la classe Livre

---
+++ AppConsoleLivres/Livre.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Livre
+    {
+        public string Titre { get; set; }
+        public string Auteur { get; set; }
+        public int NombrePages { get; set; }
+
+        public Livre(string Titre, string Auteur, int NombrePages)
+        {
+            Titre = Titre;
+            Auteur = Auteur;
+            NombrePages = NombrePages;
+        }
+        public virtual void AfficherDetail()
+        {
+            Console.WriteLine($"Titre: {Titre}, Auteur: {Auteur}, Nombre de pages: {NombrePages}");
+        }
+    }
+   
+}  
