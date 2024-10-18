6ec927e74c5ed4e4aecdfab280a821c8464e689a
Archange <archange.tshikayakalala@monboreal.ca>
2024-10-18 13:16:07 -0400

    ajout de la classe Programe et les instances

--- AppConsoleLivres/Program.cs
+++ AppConsoleLivres/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿using AppConsoleLivres;
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Revue FootRevue = new Revue("FootMercato", "Youness Boukouchi", 130, 20, 2025);
+        Revue BasketRevue = new Revue("Washington Post", "Archange Tshikaya", 200, 15, 2024);
+        Revue CuisineRevue = new Revue("Bonne Cuisine", "Gino Da Campo", 210, 18, 2008);
+
+
+
+        FootRevue.AfficherDetail();
+        BasketRevue.AfficherDetail();
+        CuisineRevue.AfficherDetail();
     }
 }
---
+++ AppConsoleLivres/Revue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AppConsoleLivres
+{
+    internal class Revue : Livre
+    {
+        public int Numero { get; set; }
+        public int Annee { get; set; }
+        public Revue(string Titre, string Auteur, int NombrePages, int Numero, int Annee) : base(Titre, Auteur, NombrePages)
+
+
+        {
+            Numero = Numero;
+            Annee = Annee;
+
+        }
+        public override void AfficherDetail()
+        {
+
+            Console.WriteLine($"Numero: {Numero}, Année: {Annee}");
+        }
+    }
+}
